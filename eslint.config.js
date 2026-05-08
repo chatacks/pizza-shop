b8ef4e5c86cebe2ddbca09a4a72893798c6e13fa
Thyago Chatack <chatack.tc@gmail.com>
2026-05-08 08:45:20 -0300

    chore: update ESLint config to use string key for simple-import-sort plugin and clean up import in main.tsx

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default defineConfig([
     plugins: {
       react,
       prettierPlugin,
-      simpleImportSort,
+      'simple-import-sort': simpleImportSort,
     },
     settings: {
       react: {
