446f3dc8e30dd09e73b030dd380ae4607e5e58f5
Thyago Chatack <chatack.tc@gmail.com>
2026-05-08 08:31:47 -0300

    chore: configure path aliases in TypeScript and Vite for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
@@ -5,7 +7,12 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react(), tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
