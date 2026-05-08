024b28392f817d759471e73b53f7cd08dc88467f
Thyago Chatack <chatack.tc@gmail.com>
2026-05-08 08:34:21 -0300

    feat: add button component ui and global styles, update Prettier config

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,9 +1,8 @@
-
-import { type Config } from "prettier";
+import { type Config } from 'prettier';
 
 const config: Config = {
   singleQuote: true,
-  plugins: ["prettier-plugin-tailwindcss"]
+  plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;
