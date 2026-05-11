0c916d4e856ec52bce4757872c37917c0cae260e
Thyago Chatack <chatack.tc@gmail.com>
2026-05-11 09:50:52 -0300

    feat: implement SignIn component with form validation and submission handling

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,48 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import * as zod from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
+const signInFormSchema = zod.object({
+  email: zod.email(),
+});
+
+type SignInFormData = zod.infer<typeof signInFormSchema>;
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(signInFormSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const handleSignIn = async (data: SignInFormData) => {
+    console.log(data);
+    try {
+      await new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+      });
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch {
+      toast.error('Credenciais inválidas');
+    }
+  };
+
   return (
     <>
       <Helmet title="Login | pizza.shop" />
@@ -19,18 +57,23 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form
+            onSubmit={handleSubmit(handleSignIn)}
+            className="space-y-4"
+          >
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input
                 id="email"
                 type="email"
+                {...register('email')}
               />
             </div>
 
             <Button
               className="w-full cursor-pointer"
               type="submit"
+              disabled={isSubmitting}
             >
               Acessar painel
             </Button>
