4afe97177488f4e2e28eba23604a95460ef2c24f
Thyago Chatack <chatack.tc@gmail.com>
2026-05-11 09:50:48 -0300

    feat: add Toaster component for enhanced notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router';
 
+import { Toaster } from './components/ui/sonner';
 import { router } from './routes';
 
 export function App() {
   return (
     <HelmetProvider>
       <RouterProvider router={router} />
+      <Toaster richColors />
     </HelmetProvider>
   );
 }
