4205946c418f8a96dc44e6f57dfe11239a063a9d
Thyago Chatack <chatack.tc@gmail.com>
2026-05-12 06:51:13 -0300

    feat: adjust routes to includes SignIn and SignUp in the AuthLayout

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app';
 import { AuthLayout } from './pages/_layouts/auth';
 import { Dashboard } from './pages/app/dashboard';
 import { SignIn } from './pages/auth/sign-in';
+import { SignUp } from './pages/auth/sign-up';
 
 export const router = createBrowserRouter([
   {
@@ -17,12 +18,16 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: '/sign-in',
+    path: '/',
     Component: AuthLayout,
     children: [
       {
-        index: true,
+        path: 'sign-in',
         Component: SignIn,
+      },
+      {
+        path: 'sign-up',
+        Component: SignUp,
       },
     ],
   },
