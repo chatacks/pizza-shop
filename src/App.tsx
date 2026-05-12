f68e7015ad66b28ceab298f36e4952a1143ad834
Thyago Chatack <chatack.tc@gmail.com>
2026-05-12 08:09:10 -0300

    feat: wrap RouterProvider with ThemeProvider for theme management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router';
 
+import { ThemeProvider } from './components/theme/theme-provider';
 import { Toaster } from './components/ui/sonner';
 import { router } from './routes';
 
 export function App() {
   return (
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
       <Toaster richColors />
     </HelmetProvider>
   );
