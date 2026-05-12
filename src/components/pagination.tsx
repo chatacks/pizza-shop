107e5177c92d7c19532b9a95e00201174c6db213
Thyago Chatack <chatack.tc@gmail.com>
2026-05-12 09:19:51 -0300

    feat: add Pagination, Select, and Table components for enhanced UI functionality

---
+++ src/components/pagination.tsx
@@ -0,0 +1,67 @@
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from 'lucide-react';
+
+import { Button } from './ui/button';
+
+interface PaginationProps {
+  pageIndex: number;
+  totalCount: number;
+  perPage: number;
+}
+
+export function Pagination({
+  pageIndex,
+  totalCount,
+  perPage,
+}: PaginationProps) {
+  const pages = Math.ceil(totalCount / perPage) || 1;
+  return (
+    <div className="flex items-center justify-between">
+      <span className="text-muted-foreground text-sm">
+        Total de {totalCount} iten(s)
+      </span>
+
+      <div className="flex items-center gap-4 lg:gap-8">
+        <div className="text-sm font-medium">
+          <div className="flex items-center gap-2">
+            <span>
+              Página {pageIndex + 1} de {pages}
+            </span>
+            <Button
+              className="h-8 w-8 p-0"
+              variant="outline"
+            >
+              <ChevronsLeft className="h-4 w-4" />
+              <span className="sr-only">Primeira página</span>
+            </Button>
+            <Button
+              className="h-8 w-8 p-0"
+              variant="outline"
+            >
+              <ChevronLeft className="h-4 w-4" />
+              <span className="sr-only">Página anterior</span>
+            </Button>
+            <Button
+              className="h-8 w-8 p-0"
+              variant="outline"
+            >
+              <ChevronRight className="h-4 w-4" />
+              <span className="sr-only">Próxima página</span>
+            </Button>
+            <Button
+              className="h-8 w-8 p-0"
+              variant="outline"
+            >
+              <ChevronsRight className="h-4 w-4" />
+              <span className="sr-only">Última página</span>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
