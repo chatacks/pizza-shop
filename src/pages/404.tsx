c428b255f60374e0444ffd8a0c4a4454a424e1a2
Thyago Chatack <chatack.tc@gmail.com>
2026-05-25 08:07:13 -0300

    feat: add NotFound component and set as ErrorBoundary in routes

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router';
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Página não encontrada</h1>
+      <p className="text-accent-foreground">
+        Voltar para o{' '}
+        <Link
+          className="text-sky-500 transition-colors hover:text-sky-600"
+          to="/"
+        >
+          Dashboard
+        </Link>
+      </p>
+    </div>
+  );
+}
