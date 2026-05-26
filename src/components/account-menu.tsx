cb113afce55f15eab3fa79c74f38510ab7d46e91
Thyago Chatack <chatack.tc@gmail.com>
2026-05-26 08:05:24 -0300

    feat: implement profile and managed restaurant fetching in AccountMenu

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,4 +1,8 @@
+import { useQuery } from '@tanstack/react-query';
 import { Building, ChevronDown, LogOut } from 'lucide-react';
+
+import { getManagedRestaurant } from '@/api/get-managed-restaurant';
+import { getProfile } from '@/api/get-profile';
 
 import { Button } from './ui/button';
 import {
@@ -9,8 +13,20 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
+import { Skeleton } from './ui/skeleton';
 
 export function AccountMenu() {
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  });
+
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+    });
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,7 +34,11 @@ export function AccountMenu() {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          Pizza Shop
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-40" />
+          ) : (
+            managedRestaurant?.name
+          )}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
@@ -28,10 +48,19 @@ export function AccountMenu() {
         className="w-56"
       >
         <DropdownMenuLabel className="flex flex-col">
-          <span className="font-semibold text-white">Thyago Chatack</span>
-          <span className="text-muted-foreground text-xs font-normal">
-            thyagochatack@teste.com.br
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-3 w-24" />
+            </div>
+          ) : (
+            <>
+              <span className="font-semibold text-white">{profile?.name}</span>
+              <span className="text-muted-foreground text-xs font-normal">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem className="cursor-pointer">
