ba21705c098bda62dc5dd06be4d32dbdf4f40e09
Thyago Chatack <chatack.tc@gmail.com>
2026-05-28 07:27:51 -0300

    feat: add getOrderDetails function for fetching order details from API

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,29 @@
+import { api } from '@/lib/axios';
+
+interface OrderItems {
+  id: string;
+  priceInCents: number;
+  quantity: number;
+  product: {
+    name: string;
+  };
+}
+
+interface GetOrderDetailsResponse {
+  id: string;
+  createdAt: string;
+  status: 'pending' | 'processing' | 'delivered' | 'canceled';
+  totalInCents: number;
+  customer: {
+    name: string;
+    phone: string | null;
+    email: string;
+  };
+  orderItems: OrderItems[];
+}
+
+export async function getOrderDetails(orderId: string) {
+  const response = await api.get<GetOrderDetailsResponse>(`/orders/${orderId}`);
+
+  return response.data;
+}
