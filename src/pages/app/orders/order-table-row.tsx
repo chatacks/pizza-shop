fb8ddb3867e55b80dd4bf907b186f34abfacd577
Thyago Chatack <chatack.tc@gmail.com>
2026-05-28 07:28:32 -0300

    feat: add state management for order details dialog in OrderTableRow component

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,6 +1,7 @@
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
 import { ArrowRight, Search, X } from 'lucide-react';
+import { useState } from 'react';
 
 import type { Order } from '@/api/get-orders';
 import { Button } from '@/components/ui/button';
@@ -15,10 +16,15 @@ interface OrderTableRowProps {
 }
 
 export function OrderTableRow({ order }: OrderTableRowProps) {
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+
   return (
     <TableRow>
       <TableCell>
-        <Dialog>
+        <Dialog
+          open={isDetailsOpen}
+          onOpenChange={setIsDetailsOpen}
+        >
           <DialogTrigger asChild>
             <Button
               className="cursor-pointer"
@@ -29,7 +35,10 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
             </Button>
           </DialogTrigger>
 
-          <OrderDetails orderId={order.orderId} />
+          <OrderDetails
+            orderId={order.orderId}
+            open={isDetailsOpen}
+          />
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
@@ -46,7 +55,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
       </TableCell>
       <TableCell className="font-medium">{order.customerName}</TableCell>
       <TableCell className="font-medium">
-        {order.total.toLocaleString('pt-BR', {
+        {(order.total / 100).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL',
         })}
