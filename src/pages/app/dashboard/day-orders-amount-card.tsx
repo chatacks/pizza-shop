32685beb52d064aef68a5b8c8ed8db8b162b66c1
Thyago Chatack <chatack.tc@gmail.com>
2026-05-29 09:45:13 -0300

    feat: add OrderTableSkeleton component to the project and implements to handle application loading states

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -4,7 +4,7 @@ import { Utensils } from 'lucide-react';
 import { getDayOrdersAmount } from '@/api/get-day-orders-amount';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
-import MetricCardSkeleton from './metric-card-skeleton';
+import { MetricCardSkeleton } from './metric-card-skeleton';
 
 export function DayOrdersAmountCard() {
   const { data: dayOrdersAmount } = useQuery({
---
+++ src/pages/app/orders/order-table-skeleton.tsx
@@ -0,0 +1,45 @@
+import { Search } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
+import { TableCell, TableRow } from '@/components/ui/table';
+
+export function OrderTableSkeleton() {
+  return Array.from({ length: 10 }).map((_, index) => {
+    return (
+      <TableRow key={index}>
+        <TableCell>
+          <Button
+            className="cursor-pointer"
+            variant="outline"
+            disabled
+          >
+            <Search className="h-3 w-3" />
+            <span className="sr-only">Detalhes do pedido</span>
+          </Button>
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-43" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-37" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-27.5" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-50" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-16" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-23" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-23" />
+        </TableCell>
+      </TableRow>
+    );
+  });
+}
