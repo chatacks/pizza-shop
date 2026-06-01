c1ca910eec6855906c073c57e43e16a34d27c3b1
Thyago Chatack <chatack.tc@gmail.com>
2026-06-01 08:44:45 -0300

    chore: setup testing environment with Vitest and Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import path from "node:path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+/// <reference types="vitest/config" />
+
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+  test: {
+    globals: true,
+    setupFiles: ['./setupTest.ts'],
+    environment: 'happy-dom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', '**/*.d.ts', '**/*.config.*', 'dist/'],
+    },
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['node_modules/', 'dist/', '.git/'],
+  },
+});
