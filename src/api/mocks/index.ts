eb8259c7a8156f925baac513edc0b6a241f42b3a
Thyago Chatack <chatack.tc@gmail.com>
2026-06-02 07:34:01 -0300

    feat: add MSW setup for mocking API responses in development

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,16 @@
+import { setupWorker } from 'msw/browser';
+
+import { env } from '@/env';
+
+import { registerRestaurantMock } from './register-restaurant-mock';
+import { signInMock } from './sign-in-mock';
+
+export const worker = setupWorker(signInMock, registerRestaurantMock);
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return;
+  }
+
+  await worker.start();
+}
