9071aae3a1b6cbde6e5da7f8794ac1039b740f73
Thyago Chatack <chatack.tc@gmail.com>
2026-06-02 08:36:15 -0300

    feat: add mock for update profile API and export UpdateProfileBody interface

---
+++ src/api/mocks/update-profile-mock.ts
@@ -0,0 +1,16 @@
+import { http, HttpResponse } from 'msw';
+
+import type { UpdateProfileBody } from '../update-profile';
+
+export const updateProfileMock = http.put<never, UpdateProfileBody>(
+  '/profile',
+  async ({ request }) => {
+    const { description, name } = await request.json();
+
+    if (name === 'Pizza Doida' && description === 'Pizza Doideira') {
+      return new HttpResponse(null, { status: 204 });
+    }
+
+    return new HttpResponse(null, { status: 400 });
+  },
+);
